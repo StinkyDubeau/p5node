d362771dc5be913394f73a8378294f71f0292b6b
StinkyDubeau <jakersunderscore09@gmail.com>
2024-01-11 00:01:15 -0500

    Add branch new. Start example sketch

--- public/sketch.js
+++ public/sketch.js
@@ -2,14 +2,33 @@
 const data = JSON.parse(sketchData);
 const parentElement = 'canvas-container';
 
+let player = {
+  xVel: 0,
+  yVel: 0,
+  xPos: 40,
+  yPos: 40,
+  dampening: 5,
+  size: 15,
+}
+
 function setup() {
   const canvas = createCanvas(400, 400);
   canvas.parent(parentElement);
-  
+  rectMode(CENTER);
+
   console.log(`Project '${data.projectName}' loaded successfully. (~:`);
 }
 
 function draw() {
   background(220);
   text(data.testText, 5, 20);
+  rect(player.xPos, player.yPos, player.size, player.size);
+
+  mouseInput();
+}
+
+function mouseInput() {
+  player.xVel = movedX;
+  player.yVel = movedY;
+
 }
